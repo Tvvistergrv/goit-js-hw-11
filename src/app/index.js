fbe8c9868d25ad5a81ba1297e92e38d3b18a1898
Roman Gorobets <Gorobetcrv@gmail.com>
2023-06-01 23:14:46 +0300

    fixed

--- src/app/index.js
+++ src/app/index.js
@@ -131,7 +131,7 @@ const onSubmitClick = async event => {
 const onLoadMore = async () => {
   pixaby.incrementPage();
 
-  if (!pixaby.hasMorePhotos) {
+  if (!pixaby.hasMorePhotos()) {
     refs.btnLoadMore.classList.add('is-hidden');
     Notify.info("We're sorry, but you've reached the end of search results.");
     notifyInit;
